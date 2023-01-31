c61abf9b73f3c39b42d71feb71b36e2720e4746c
Andrew Owens <andrrowens@gmail.com>
2023-01-31 11:53:30 -0700

    commit tree afba87117a1465529983b3ef280acc66b7c84024

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :spices, except: [:show]
+  #get '/spices', to: "spices#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+
 end
